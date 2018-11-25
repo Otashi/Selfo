eac50c37c78dc66bc9b200046dbcf9b06319c04c
Luis Hong Wu Wang <luishongwuwang@Luiss-MacBook-Pro.local>
2018-11-25 19:16:05 +0100

    QR Scanner

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
+import { QRScanner } from '@ionic-native/qr-scanner';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { HomePage } from './home';
   imports: [
     IonicPageModule.forChild(HomePage),
   ],
+  providers: [QRScanner],
   exports: [HomePage]
 })
 export class HomePageModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../../services/auth.service'
 import { LoginPage } from '../login/login';
 
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
+
 /**
  * Generated class for the HomePage page.
  *
@@ -17,7 +20,8 @@ import { LoginPage } from '../login/login';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthService, 
+    private qrScanner: QRScanner) {
     if(!this.auth.authenticated){
       console.log("Not logged");
       this.navCtrl.setRoot(LoginPage);
@@ -29,6 +33,32 @@ export class HomePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  scanQR(){
+    this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+
+
+       // start scanning
+       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+
+         this.qrScanner.hide(); // hide camera preview
+         scanSub.unsubscribe(); // stop scanning
+       });
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
   }
 
 }
